d7547c15b39aecdf283b67a9631124efee709071
Matt Moore <mattmoore@carbonhelix.com>
2019-10-11 04:06:43 +0000

    Futures

--- futures/Futures.scala
+++ futures/Futures.scala
@@ -1,16 +1,46 @@
 import scala.concurrent._
-import scala.concurrent.duration.Duration
-import scala.concurrent.{Await, Future}
+import scala.concurrent.Future
 import scala.util.{Success, Failure}
 import ExecutionContext.Implicits.global
 
 object Futures {
   def main(args: Array[String]) {
-    val x = Future { "I'm the future value!" }
-    x.onComplete {
-      case Success(value) => println(s"The value is: $value")
-      case Failure(e) => println("Failed")
+    successfulFutures
+    println("Should see no futures print out past here.")
+    failedFutures
+  }
+
+  def successfulFutures {
+    val x = Future { "x future value" }
+    val y = Future { "y future value" }
+    val z = Future { "z future value" }
+    for {
+      xValue <- x
+      yValue <- y
+      zValue <- z
+    } yield {
+      // All of these futures are successful so we'll see them print out.
+      println(xValue)
+      println(yValue)
+      println(zValue)
     }
-    Await.ready(x, Duration.Inf)
+  }
+
+  def failedFutures {
+    val x = Future { "x future value" }
+    // This future will fail
+    val y = Future { throw new Exception("y future value") }
+    val z = Future { "z future value" }
+    for {
+      xValue <- x
+      yValue <- y
+      zValue <- z
+    } yield {
+      // Because the y future failed, none of these will print.
+      // The for comprehension requires complete success accross all futures.
+      println(xValue)
+      println(yValue)
+      println(zValue)
+    }
   }
 }
